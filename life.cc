3d3b019d9e66bb86aff1ec26e7ed268a299f08b8
Datong Sun <dsun@iastate.edu>
2013-11-08 23:09:24 -0600

    Implemented most of the command line arguments.

--- life.cc
+++ life.cc
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <cstdio>
 #include "utils.h"
 #include "miner_parser.h"
 #include "simulator.h"
 
 using namespace std;
+
+enum OutputFormat {VISUAL, MINER };
 
 void show_terrain(Simulator &sim, unsigned char alive, unsigned char dead, pair<int, int> yrange, pair<int, int> xrange)
 {
@@ -35,14 +40,141 @@ void show_terrain(Simulator &sim, unsigned char alive, unsigned char dead, pair<
     }
 }
 
+pair<int, int> extract_pair(const char *s)
+{
+    pair<int, int> result;
+    int ret = sscanf(s, "%d..%d", &result.first, &result.second);
+    if (ret != 2)
+        FATAL("invalid pair format %s", s);
+
+    return result;
+}
+
 int main(int argc, char *argv[])
 {
+    opterr = 0; // mute getopt error meesage
+    int c;
+    int generation{0};
+    OutputFormat output_format = VISUAL;
+    bool terrain_x_override{false}, terrain_y_override{false};
+    bool window_x_override{false}, window_y_override{false};
+    pair<int, int> terrain_y_range, terrain_x_range, window_y_range, window_x_range;
+
+    for (int i = 1; i < argc; i++)
+    {
+        string arg{argv[i]};
+
+        if (arg == "-ty")
+        {
+            argv[i][1] = 'q';
+            argv[i][2] = '\0';
+        }
+        else if (arg == "-tx")
+        {
+            argv[i][1] = 'w';
+            argv[i][2] = '\0';
+        }
+        else if (arg == "-wy")
+        {
+            argv[i][1] = 'e';
+            argv[i][2] = '\0';
+        }
+        else if (arg == "-wx")
+        {
+            argv[i][1] = 'r';
+            argv[i][2] = '\0';
+        }
+    }
+
+    while ((c = getopt(argc, argv, "fg:hq:w:ve:r:")) != -1)
+    {
+        switch (c)
+        {
+            case 'h':
+                cerr << "Usage: life [-fhv] [-g n] [-tx l..h] [-ty l..h] [-wx l..h] [-wy l..h] [filename]" << endl << endl
+                     << "  -f        Output should be in the same file format as Appendix A" << endl
+                     << "  -g n      Specify the desired generation number. If omitted, the default should be n = 0" << endl
+                     << "  -h        Display this message and quit" << endl
+                     << "  -tx l..h  Set the x range of the terrain; overrides values specified in the input file" << endl
+                     << "  -ty l..h  Set the y range of the terrain; overrides values specified in the input file" << endl
+                     << "  -v        Output should be visual: an ASCII display of the terrain with appropriate characters for the" << endl
+                     << "            dead and alive cells" << endl
+                     << "  -wx l..h  Set the x range of the output window; otherwise this defaults to the x range of the terrain" << endl
+                     << "  -wy l..h  Set the y range of the output window; otherwise this defaults to the x range of the terrain" << endl
+                     << endl;
+                return 0;
+
+            case 'g':
+                generation = atoi(optarg);
+                if (generation < 0)
+                    FATAL("generation number must equal or greater than zero");
+                break;
+
+            case 'f':
+                output_format = MINER;
+                break;
+
+            case 'v':
+                output_format = VISUAL;
+                break;
+
+            case 'q': // ty
+                terrain_y_override = true;
+                terrain_y_range = extract_pair(optarg);
+                break;
+
+            case 'w': // tx
+                terrain_x_override = true;
+                terrain_x_range = extract_pair(optarg);
+                break;
+
+            case 'e': // wy
+                window_y_override = true;
+                window_y_range = extract_pair(optarg);
+                break;
+
+            case 'r': // wx
+                window_x_override = true;
+                window_x_range = extract_pair(optarg);
+                break;
+
+            case '?':
+                if (optopt == 'q')
+                    cerr << "need argument for option -ty" << endl;
+                else if (optopt == 'w')
+                    cerr << "need argument for option -tx" << endl;
+                else if (optopt == 'e')
+                    cerr << "need argument for option -wy" << endl;
+                else if (optopt == 'r')
+                    cerr << "need argument for option -wx" << endl;
+                else
+                    cerr << "unknown argument -" << (char) optopt << endl;
+                return 1;
+        }
+    }
+
     std::string in_buffer;
 
-    int ch;
-    while ((ch = cin.get()) != EOF)
+    if (optind < argc) // read from argument
     {
-        in_buffer.push_back(ch);
+        ifstream in_file{argv[optind]};
+        if (!in_file.is_open())
+            FATAL("Could not open file %s", argv[optind]);
+
+        int ch;
+        while ((ch = in_file.get()) != EOF)
+        {
+            in_buffer.push_back(ch);
+        }
+        in_file.close();
+    }
+    else
+    {
+        int ch;
+        while ((ch = cin.get()) != EOF)
+        {
+            in_buffer.push_back(ch);
+        }
     }
 
     Struct input = parse_miner_string(in_buffer);
@@ -74,10 +206,31 @@ int main(int argc, char *argv[])
             FATAL("missing Xrange and Yrange of Window or incorrect type");
     }
 
-    pair<int, int> terrain_x_range{terrain_range["Xrange"]};
-    pair<int, int> terrain_y_range{terrain_range["Yrange"]};
-    pair<int, int> window_y_range{window_range["Yrange"]};
-    pair<int, int> window_x_range{window_range["Xrange"]};
+    if (!terrain_y_override)
+        terrain_y_range = (terrain_range["Yrange"]);
+    if (!terrain_x_override)
+        terrain_x_range = (terrain_range["Xrange"]);
+
+    if (!window_y_override)
+    {
+        if (terrain_y_override)
+            window_y_range = terrain_y_range;
+        else
+            window_y_range = (window_range["Yrange"]);
+    }
+    if (!window_x_override)
+    {
+        if (terrain_x_override)
+            window_x_range = terrain_x_range;
+        else
+            window_x_range = (window_range["Xrange"]);
+    }
+
+    if (terrain_y_range.first > terrain_y_range.second || terrain_x_range.first > terrain_x_range.second)
+        FATAL("terrain value must be low..high, got high..low");
+
+    if (window_y_range.first > window_y_range.second || window_x_range.first > window_x_range.second)
+        FATAL("window value must be low..high, got high..low");
 
     Simulator sim{terrain_y_range.first, terrain_y_range.second,
                   terrain_x_range.first, terrain_x_range.second};
@@ -120,11 +273,50 @@ int main(int argc, char *argv[])
 
     unsigned char alive{chars["Alive"]}, dead{chars["Dead"]};
 
-    unsigned int iter{0};
-    cout << "genetation " << iter << endl;
-    show_terrain(sim, alive, dead, window_y_range, window_x_range);
-    sim.simulate();
-    show_terrain(sim, alive, dead, window_y_range, window_x_range);
+    for (int i = 0; i < generation; i++)
+        sim.simulate();
+
+    if (output_format == VISUAL)
+    {
+        show_terrain(sim, alive, dead, window_y_range, window_x_range);
+    }
+    else
+    {
+        Struct tmp;
+        
+        // write terrain struct
+        tmp["Xrange"] = terrain_x_range;
+        tmp["Yrange"] = terrain_y_range;
+        input["Terrain"] = tmp;
+
+        // write window struct
+        tmp = Struct();
+        tmp["Xrange"] = window_x_range;
+        tmp["Yrange"] = window_y_range;
+        input["Window"] = tmp;
+
+        List alive;
+
+        for (auto iy = sim.cbegin(); iy != sim.cend(); ++iy)
+        {
+            int y = terrain_y_range.first + (iy - sim.cbegin());
+            for (auto ix = iy->cbegin(); ix != iy->cend(); ++ix)
+            {
+                int x = terrain_x_range.first + (ix - iy->cbegin());
+                if (*ix) // alive
+                {
+                    alive.push_back(pair<int, int>(x, y));
+                }
+            }
+        }
+
+        // alive cells
+        tmp = Struct();
+        tmp["Alive"] = alive;
+        input["Initial"] = tmp;
+
+        cout << "Life = " << input << ';' << endl;
+    }
 
     return 0;
 }
