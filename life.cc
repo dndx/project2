3c445072f42e03f74a65b5512f5c870b33db7135
Datong Sun <dsun@iastate.edu>
2013-11-08 18:14:44 -0600

    Added error checking for life program, add window range filter, add dead and life choose.

--- life.cc
+++ life.cc
@@ -1,24 +1,34 @@
 #include <iostream>
-#include <unistd.h>
 #include "utils.h"
 #include "miner_parser.h"
 #include "simulator.h"
 
 using namespace std;
 
-void show_terrain(Simulator &sim)
+void show_terrain(Simulator &sim, unsigned char alive, unsigned char dead, pair<int, int> yrange, pair<int, int> xrange)
 {
+    yrange.first = sim.to_y(yrange.first);
+    yrange.second = sim.to_y(yrange.second);
+    xrange.first = sim.to_x(xrange.first);
+    xrange.second = sim.to_x(xrange.second);
+
     for (auto iy = sim.crbegin(); iy != sim.crend(); ++iy)
     {
+        int y = iy - sim.crbegin();
+        if (y < yrange.first || y > yrange.second)
+            continue;
         for (auto ix = iy->cbegin(); ix != iy->cend(); ++ix)
         {
+            int x = ix - iy->cbegin();
+            if (x < xrange.first || x > xrange.second)
+                continue;
             if (*ix) // alive
             {
-                cout << '@';
+                cout << alive;
             }
             else // dead
             {
-                cout << '-';
+                cout << dead;
             }
         }
         cout << endl;
@@ -37,31 +47,47 @@ int main(int argc, char *argv[])
 
     Struct input = parse_miner_string(in_buffer);
 
-    if (!input.count("Terrain"))
+    if (input["Terrain"].get_type() != STRUCT)
     {
-        FATAL("missing Terrain definition");
+        FATAL("missing Terrain definition or incorrect value type for Terrain");
     }
 
-    Struct terrian_range{input["Terrain"]};
+    Struct terrain_range{input["Terrain"]};
+
+    if (terrain_range["Xrange"].get_type() != RANGE || terrain_range["Yrange"].get_type() != RANGE)
+    {
+        FATAL("Xrange and Yrange for Terrain must be range type");
+    }
+
     Struct window_range;
 
     if (!input.count("Window"))
-        window_range = terrian_range;
+    {
+        window_range = terrain_range;
+    }
     else
+    {
+        if (input["Window"].get_type() != STRUCT)
+            FATAL("incorrect type of Window");
         window_range = input["Window"];
+        if (window_range["Xrange"].get_type() != RANGE || window_range["Yrange"].get_type() != RANGE)
+            FATAL("missing Xrange and Yrange of Window or incorrect type");
+    }
 
-    pair<int, int> terrian_x_range{terrian_range["Xrange"]};
-    pair<int, int> terrian_y_range{terrian_range["Yrange"]};
+    pair<int, int> terrain_x_range{terrain_range["Xrange"]};
+    pair<int, int> terrain_y_range{terrain_range["Yrange"]};
+    pair<int, int> window_y_range{window_range["Yrange"]};
+    pair<int, int> window_x_range{window_range["Xrange"]};
 
-    Simulator sim{terrian_y_range.first, terrian_y_range.second,
-                  terrian_x_range.first, terrian_x_range.second};
+    Simulator sim{terrain_y_range.first, terrain_y_range.second,
+                  terrain_x_range.first, terrain_x_range.second};
 
-    if (!input.count("Initial"))
-        FATAL("missing Initial defination");
+    if (input["Initial"].get_type() != STRUCT)
+        FATAL("missing Initial defination or incorrect value type for Initial");
 
     Struct initial{input["Initial"]};
-    if (!initial.count("Alive"))
-        FATAL("missing Alive definition");
+    if (initial["Alive"].get_type() != LIST)
+        FATAL("missing Alive or incorrect type");
 
     List initial_alive{initial["Alive"]};
 
@@ -69,26 +95,36 @@ int main(int argc, char *argv[])
     {
         pair<int, int> coord{initial_alive[i]};
 
-        if (coord.first < terrian_x_range.first || coord.first > terrian_x_range.second)
+        if (coord.first < terrain_x_range.first || coord.first > terrain_x_range.second)
             FATAL("initial cell x coordinate out of bound, got %d", coord.first);
 
-        if (coord.second < terrian_y_range.first || coord.second > terrian_y_range.second)
+        if (coord.second < terrain_y_range.first || coord.second > terrain_y_range.second)
             FATAL("initial cell y coordinate out of bound, got %d", coord.second);
 
         sim.set_status(coord.second, coord.first, true);
     }
 
-    
+    if (window_y_range.first < terrain_y_range.first || window_y_range.second > terrain_y_range.second)
+        FATAL("Y range of Window out of bound");
+
+    if (window_x_range.first < terrain_x_range.first || window_x_range.second > terrain_x_range.second)
+        FATAL("X range of Window out of bound");
+
+    if (input["Chars"].get_type() != STRUCT)
+        FATAL("missing Chars definition or incorrect value type");
+
+    Struct chars{input["Chars"]};
+
+    if (chars["Alive"].get_type() != INTEGER || chars["Dead"].get_type() != INTEGER)
+        FATAL("missing Alive and Dead definition or incorrect value type");
+
+    unsigned char alive{chars["Alive"]}, dead{chars["Dead"]};
+
     unsigned int iter{0};
     cout << "genetation " << iter << endl;
-    show_terrain(sim);
-    while (++iter)
-    {
-        sim.simulate();
-        cout << "genetation " << iter << endl;
-        show_terrain(sim);
-        //sleep(1);
-    }
+    show_terrain(sim, alive, dead, window_y_range, window_x_range);
+    sim.simulate();
+    show_terrain(sim, alive, dead, window_y_range, window_x_range);
 
     return 0;
 }
--- simulator.h
+++ simulator.h
@@ -10,14 +10,14 @@ class Simulator {
         void simulate();
         void set_status(int y, int x, bool status);
         bool get_status(int y, int x);
+        unsigned int to_x(int x);
+        unsigned int to_y(int y);
         std::vector<std::vector<bool>>::const_iterator cbegin();
         std::vector<std::vector<bool>>::const_iterator cend();
         std::vector<std::vector<bool>>::const_reverse_iterator crbegin();
         std::vector<std::vector<bool>>::const_reverse_iterator crend();
 
     private:
-        unsigned int to_x(int x);
-        unsigned int to_y(int y);
         unsigned int count_live_neighbors(int y, int x); // x and y should be converted
 
     private:
